1ea436a71c60e04388dc065e564eed4f2adefcd0
Briar <briar.j.mills@gmail.com>
2025-03-07 13:46:58 -0500

    First draft

--- src/hdx/scraper/wfp_rainfall/wfp_rainfall.py
+++ src/hdx/scraper/wfp_rainfall/wfp_rainfall.py
@@ -2,16 +2,27 @@
 """wfp-rainfall scraper"""
 
 import logging
+from datetime import timedelta
+from typing import List, Optional
 
 from hdx.api.configuration import Configuration
 from hdx.api.utilities.hdx_error_handler import HDXErrorHandler
 from hdx.data.dataset import Dataset
 from hdx.location.adminlevel import AdminLevel
+from hdx.location.country import Country
 from hdx.scraper.framework.utilities.hapi_admins import complete_admins
-from hdx.utilities.dateparse import iso_string_from_datetime, parse_date_range
+from hdx.utilities.dateparse import iso_string_from_datetime, parse_date
 from hdx.utilities.retriever import Retrieve
+from kalendar import Dekad
 
 logger = logging.getLogger(__name__)
+
+
+_TIME_PERIODS = {
+    "f": "dekad",
+    "1": "1-month",
+    "3": "3-month",
+}
 
 
 class WFPRainfall:
@@ -27,7 +38,7 @@ class WFPRainfall:
         self._temp_dir = temp_dir
         self._error_handler = error_handler
         self._admins = []
-        self.data = {}
+        self.data = []
         self.dates = []
 
     def get_pcodes(self) -> None:
@@ -38,8 +49,80 @@ class WFPRainfall:
             admin.load_pcode_formats()
             self._admins.append(admin)
 
-    def download_data(self) -> None:
+    def download_data(self, countryiso3s: Optional[List] = None) -> None:
         self.get_pcodes()
+        if not countryiso3s:
+            countryiso3s = [key for key in Country.countriesdata()["countries"]]
+        for countryiso3 in countryiso3s:
+            dataset_name = f"{countryiso3.lower()}-rainfall-subnational"
+            dataset = Dataset.read_from_hdx(dataset_name)
+            if not dataset:
+                continue
+            dataset_id = dataset["id"]
+            hrp = "Y" if Country.get_hrp_status_from_iso3(countryiso3) else "N"
+            gho = "Y" if Country.get_gho_status_from_iso3(countryiso3) else "N"
+
+            resources = [r for r in dataset.get_resources() if "5ytd" in r["name"]]
+            if len(resources) == 0:
+                self._error_handler.add_message(
+                    "Rainfall",
+                    dataset_name,
+                    "Could not find resource",
+                    message_type="warning",
+                )
+                continue
+            resource = resources[0]
+            resource_id = resource["id"]
+            headers, rows = self._retriever.get_tabular_rows(
+                resource["url"], dict_form=True
+            )
+            for row in rows:
+                if "#" in row["ADM2_PCODE"]:
+                    continue
+
+                provider_adm_names = ["", ""]
+                adm_codes = ["", row["ADM2_PCODE"]]
+                adm_names = ["", ""]
+                adm_level, warnings = complete_admins(
+                    self._admins,
+                    countryiso3,
+                    provider_adm_names,
+                    adm_codes,
+                    adm_names,
+                )
+                errors = []
+
+                start_date = parse_date(row["date"])
+                dekad = Dekad.fromdatetime(start_date)
+                end_date = (dekad + 1).todate() - timedelta(days=1)
+                start_date = iso_string_from_datetime(start_date)
+                end_date = end_date.isoformat()
+
+                for time_header, time_period in _TIME_PERIODS.items():
+                    hapi_row = {
+                        "location_code": countryiso3,
+                        "has_hrp": hrp,
+                        "in_gho": gho,
+                        "provider_admin1_name": provider_adm_names[0],
+                        "provider_admin2_name": provider_adm_names[1],
+                        "admin1_code": adm_codes[0],
+                        "admin1_name": adm_names[0],
+                        "admin2_code": adm_codes[1],
+                        "admin2_name": adm_names[1],
+                        "admin_level": 2,
+                        "wfp_id": row["adm2_id"],
+                        "time_period": time_period,
+                        "rainfall": row[f"r{time_header}h"],
+                        "rainfall_long_term_average": row[f"r{time_header}h_avg"],
+                        "rainfall_anomaly_pct": row[f"r{time_header}q"],
+                        "reference_period_start": start_date,
+                        "reference_period_end": end_date,
+                        "dataset_hdx_id": dataset_id,
+                        "resource_hdx_id": resource_id,
+                        "warning": "|".join(warnings),
+                        "error": "|".join(errors),
+                    }
+                    self.data.append(hapi_row)
 
     def generate_dataset(self) -> Dataset:
         dataset = Dataset(

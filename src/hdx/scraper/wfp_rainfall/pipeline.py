d0d82b0c2d7044b3095939833c462f01fb364dde
mcarans <rans@email.com>
2026-06-03 15:08:17 +1200

    Use pipelineutils

--- src/hdx/scraper/wfp_rainfall/pipeline.py
+++ src/hdx/scraper/wfp_rainfall/pipeline.py
@@ -10,7 +10,7 @@ from hdx.api.utilities.hdx_error_handler import HDXErrorHandler
 from hdx.data.dataset import Dataset
 from hdx.location.adminlevel import AdminLevel
 from hdx.location.country import Country
-from hdx.scraper.framework.utilities.hapi_admins import complete_admins
+from hdx.pipelineutils.hapi_admins import complete_admins
 from hdx.utilities.dateparse import iso_string_from_datetime, parse_date
 from hdx.utilities.dictandlist import dict_of_lists_add
 from hdx.utilities.downloader import DownloadError
